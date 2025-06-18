cfdf7fefe276a6cbfe3c223ada26a7b80ac14750
Nick Peng <gibson7780@gmail.com>
2025-06-18 21:14:21 +0800

    adjusted domain to cloud domain

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -1,7 +1,8 @@
 import useAuthStore from '@/state/authstore';
 import axios from 'axios';
 
-const axiosInstance = axios.create({ baseURL: 'http://localhost:2469/api' });
+// const axiosInstance = axios.create({ baseURL: 'http://localhost:2469/api' });
+const axiosInstance = axios.create({ baseURL: 'http://134.199.210.228:2469/api' });
 
 axiosInstance.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
