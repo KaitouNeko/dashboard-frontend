6a833575691513e323a52f95b98fa913a2baeee1
Nick Peng <gibson7780@gmail.com>
2025-06-22 17:49:15 +0800

    remove background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/components/login-form';
 
 export default function Home() {
   return (
-    <div className="grid bg-black grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-sora)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-sora)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <LoginForm />
